74976492e2673722746991b38aa9e496dde91b4f
constantinDM <constantin_md@hotmail.com>
2024-11-06 03:36:54 -0800

    it workss

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 const API_URL = '/api/proxy';
-// 
+
 function App() {
   const messagesEndRef = useRef(null);
   const [input, setInput] = useState('');
